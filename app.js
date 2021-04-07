481f9d64577f4a4eb0a60776b9e18943c2bdaad2
isama22 <emarti17@artic.edu>
2021-04-07 13:11:42 -0500

    added ig link and hover on avatar

---
+++ app.js
@@ -0,0 +1,12 @@
+var tooltip = document.querySelectorAll('.hover-div');
+
+document.addEventListener('mousemove', fn, false);
+
+function fn(e) {
+    for (var i=tooltip.length; i--;) {
+        tooltip[i].style.left = e.pageX + 'px';
+        tooltip[i].style.top = e.pageY + 'px';
+    }
+}
+
+console.log('working')
